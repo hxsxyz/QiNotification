7090b303c3b3ac241c3f28cf7c30ba52ff169124
wangdacheng <>
2018-09-20 15:11:09 +0800

    远程推送
    添加远程推送相关功能代码

--- QiNotification/AppDelegate.m
+++ QiNotification/AppDelegate.m
@@ -28,14 +28,14 @@
     [_window setRootViewController:nav];
     [_window makeKeyAndVisible];
     
-    [self registerLocalNotification];
+    //[self registerLocalNotification];
+    [self registerRemoteNotifications];
     
     return YES;
 }
 
 - (void)registerLocalNotification {
-    
-    // 系统版本 >= iOS10
+
     if (@available(iOS 10.0, *)) {
         UNUserNotificationCenter *center = [UNUserNotificationCenter currentNotificationCenter];
         center.delegate = self;
@@ -50,12 +50,48 @@
     }
 }
 
+
+- (void)registerRemoteNotifications
+{
+    if (@available(iOS 10.0, *)) {
+        UNUserNotificationCenter *center = [UNUserNotificationCenter currentNotificationCenter];
+        center.delegate = self;
+        [center requestAuthorizationWithOptions:(UNAuthorizationOptionBadge | UNAuthorizationOptionSound | UNAuthorizationOptionAlert | UNAuthorizationOptionCarPlay) completionHandler:^(BOOL granted, NSError *_Nullable error) {
+            if (!error) {
+                NSLog(@"request authorization succeeded!");
+                [[UIApplication sharedApplication] registerForRemoteNotifications];
+            } else {
+                NSLog(@"request authorization failed!");
+            }
+        }];
+    } else if ([[[UIDevice currentDevice] systemVersion] floatValue] >= 8.0) {
+        UIUserNotificationType types = (UIUserNotificationTypeAlert | UIUserNotificationTypeSound | UIUserNotificationTypeBadge);
+        UIUserNotificationSettings *settings = [UIUserNotificationSettings settingsForTypes:types categories:nil];
+        [[UIApplication sharedApplication] registerUserNotificationSettings:settings];
+        [[UIApplication sharedApplication] registerForRemoteNotifications];
+    } else {
+        UIRemoteNotificationType apn_type = (UIRemoteNotificationType)(UIRemoteNotificationTypeAlert |
+                                                                       UIRemoteNotificationTypeSound |
+                                                                       UIRemoteNotificationTypeBadge);
+        [[UIApplication sharedApplication] registerForRemoteNotificationTypes:apn_type];
+    }
+}
+
+
 - (void)application:(UIApplication *)application didRegisterUserNotificationSettings:(UIUserNotificationSettings *)notificationSettings {
     NSLog(@"didRegisterUserNotificationSettings");
 }
 
 - (void)application:(UIApplication *)application didReceiveLocalNotification:(UILocalNotification *)notification {
-    NSLog(@"app收到本地推送:%@", notification.userInfo);
+    NSLog(@"app收到本地推送(didReceiveLocalNotification:):%@", notification.userInfo);
+}
+
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
+    
+    // 获取并处理deviceToken
+    NSString *token = [[deviceToken description] stringByTrimmingCharactersInSet:[NSCharacterSet characterSetWithCharactersInString:@"<>"]];
+    token = [token stringByReplacingOccurrencesOfString:@" " withString:@""];
+    NSLog(@"DeviceToken:%@\n", token);
 }
 
 // 注：iOS10以上，如果不使用UNUserNotificationCenter，将走此回调方法
@@ -64,9 +100,9 @@
     // iOS6及以下系统
     if (userInfo) {
         if ([UIApplication sharedApplication].applicationState == UIApplicationStateActive) {// app位于前台通知
-            NSLog(@"app位于前台通知:%@", userInfo);
+            NSLog(@"app位于前台通知(didReceiveRemoteNotification:):%@", userInfo);
         } else {// 切到后台唤起
-            NSLog(@"app位于后台通知:%@", userInfo);
+            NSLog(@"app位于后台通知(didReceiveRemoteNotification:):%@", userInfo);
         }
     }
 }
@@ -76,9 +112,9 @@
     // iOS7及以上系统
     if (userInfo) {
         if ([UIApplication sharedApplication].applicationState == UIApplicationStateActive) {// app位于前台通知
-            NSLog(@"app位于前台通知:%@", userInfo);
+            NSLog(@"app位于前台通知(didReceiveRemoteNotification:fetchCompletionHandler:):%@", userInfo);
         } else {// 切到后台唤起
-            NSLog(@"app位于后台通知:%@", userInfo);
+            NSLog(@"app位于后台通知(didReceiveRemoteNotification:fetchCompletionHandler:):%@", userInfo);
         }
     }
     completionHandler(UIBackgroundFetchResultNewData);
@@ -92,7 +128,7 @@
 API_AVAILABLE(ios(10.0)){
     NSDictionary * userInfo = notification.request.content.userInfo;
     if (userInfo) {
-        NSLog(@"app位于前台通知:%@", userInfo);
+        NSLog(@"app位于前台通知(willPresentNotification:):%@", userInfo);
     }
     completionHandler(UNNotificationPresentationOptionBadge|UNNotificationPresentationOptionSound|UNNotificationPresentationOptionAlert);;
 }
@@ -102,7 +138,7 @@ API_AVAILABLE(ios(10.0)){
 API_AVAILABLE(ios(10.0)){
     NSDictionary * userInfo = response.notification.request.content.userInfo;
     if (userInfo) {
-        NSLog(@"点击通知进入App时触发:%@", userInfo);
+        NSLog(@"点击通知进入App时触发(didReceiveNotificationResponse:):%@", userInfo);
     }
     completionHandler();
 }
@@ -110,3 +146,17 @@ API_AVAILABLE(ios(10.0)){
 
 
 @end
+
+
+
+
+//// apsModel示例
+/**
+ {"aps":{"alert":{"title":"通知的title","subtitle":"通知的subtitle","body":"通知的body","title-loc-key":"TITLE_LOC_KEY","title-loc-args":["t_01","t_02"],"loc-key":"LOC_KEY","loc-args":["l_01","l_02"]},"sound":"sound01.wav","badge":1,"mutable-content":1,"category": "realtime"},"msgid":"123"}
+ */
+
+/*
+ 特殊说明：
+ 1. APNS去掉alert、badge、sound字段实现静默推送，增加增加字段"content-available":1，也可以在后台做一些事情。
+ 2. mutable-content这个键值为1，说明此条推送可以被 Service Extension 进行更改。
+ */
